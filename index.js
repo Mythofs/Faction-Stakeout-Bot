ef8ef497e5ba9451933bbee841563121a1e6879a
Mythofs <bxawesome1@gmail.com>
2026-07-05 18:38:52 -0400

    Added stakeout

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-const factionActivityStore = require('./factionActivityStore.js')
-const individualActivityStore = require('./individualActivityStore.js');
-const { startActivityInterval } = require('./commands/monitor/startmonitor.js');
 require('dotenv').config();
-
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -41,87 +37,5 @@ for(const file of eventFiles) {
 }
 
 client.once('clientReady', async () => {
-    const apiKey = process.env.API_KEY;
-    const channel = await client.channels.fetch(process.env.CHANNEL_ID);
-    startWarInterval(apiKey, channel);
 });
 client.login(process.env.TOKEN);
-
-async function startWarInterval(apiKey, channel)
-{
-    try {
-        const id = await checkForWar(apiKey, channel);
-        if(id != -1) {
-            if(factionActivityStore.has(id))
-                factionActivityStore.get(id).clear();
-            startActivityInterval(apiKey, id, channel);
-            startActivityInterval(apiKey, Number(process.env.FAC_ID), channel);
-            return;
-        }
-        const intervalId = setInterval(async () => {
-            const id = await checkForWar(apiKey, channel);
-            if(id != -1) {
-                clearInterval(intervalId);
-                startActivityInterval(apiKey, id, channel);
-                startActivityInterval(apiKey, Number(process.env.FAC_ID), channel);
-                waitForWarEnd(apiKey, channel);
-            }
-        }, 3600000);
-    }
-    catch(e) { 
-        channel.send(`Error while running war interval ${e}`);
-        console.log(`Error while running war interval ${e}`);
-    }
-}
-async function checkForWar(apiKey, channel)
-{
-    try {
-        const warData = await safeFetch(`https://api.torn.com/v2/faction/wars?comment=Activity%20Tracker%20Bot&key=${apiKey}`, channel);
-        if(Array.isArray(warData.wars.ranked) || warData.wars.ranked.end != null)
-            return -1;
-        return warData.wars.ranked.factions.find(fac => fac.id != process.env.FAC_ID).id;
-    }
-    catch(e) { 
-        channel.send(`Error while checking for war ${e}`);
-        console.log(`Error while checking for war ${e}`);
-        return -1;
-    }
-}
-async function waitForWarEnd(apiKey, channel)
-{
-    try {
-        const warData = await safeFetch(`https://api.torn.com/v2/faction/wars?comment=Activity%20Tracker%20Bot&key=${apiKey}`, channel);
-        if(Array.isArray(warData.wars.ranked) || warData.wars.ranked.end != null) {
-            startWarInterval(apiKey, channel);
-            return;
-        }
-        const intervalId = setInterval(async () => {
-            const warData = await safeFetch(`https://api.torn.com/v2/faction/wars?comment=Activity%20Tracker%20Bot&key=${apiKey}`, channel);
-            if(Array.isArray(warData.wars.ranked) || warData.wars.ranked.end != null) {
-                clearInterval(intervalId);
-                startWarInterval(apiKey, channel);
-            }
-        })
-    }
-    catch(e) {
-        channel.send(`Error while starting war end interval ${e}`);
-        console.log(`Error while starting war end interval ${e}`);
-    }
-}
-async function safeFetch(url, channel) {
-    let response;
-    try {
-        response = await fetch(url);
-    } catch (error) {
-        channel.send(`Error while fetching ${url}, ${error}`);
-    }
-    let data;
-    try {
-        data = await response.json();
-    } catch(error) {
-        channel.send(`Invalid JSON from ${url}, ${error}`);
-    }
-    if (!response.ok)
-        channel.send(`Error from ${url}: ${JSON.stringify(data)}`);
-    return data;
-}
